5be6bdbd1f196047eeaaaf6c72856a6091468032
Gaston <gaston.quittner@gmail.com>
2023-01-29 17:54:20 -0600

    React Navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,29 @@
+import { AppBar, Box, Tab, Tabs, Toolbar, Typography } from "@mui/material";
+import { useState } from "react";
+
 const Header = () => {
-    return <div>Header</div>;
+    const [value, setValue] = useState();
+
+    return (
+        <div>
+            <AppBar>
+                <Toolbar>
+                    <Typography variant="h3">MERN Auth</Typography>
+                    <Box sx={{ marginLeft: "auto" }}>
+                        <Tabs
+                            textColor="inherit"
+                            indicatorColor="secondary"
+                            value={value}
+                            onChange={(e, val) => setValue(val)}
+                        >
+                            <Tab label="Login" />
+                            <Tab label="Sign Up" />
+                        </Tabs>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 };
 
 export default Header;
