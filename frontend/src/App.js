bf3ddf91522616403e195620d6e08f475f06d22e
Gaston <gaston.quittner@gmail.com>
2023-01-29 19:01:45 -0600

    Login UI and API call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
                 <Routes>
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<Signup />} />
-                    <Route path="/user" element={<Home />} />
+                    <Route path="/" element={<Home />} />
                 </Routes>
             </main>
         </>
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,82 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 const Login = () => {
-    return <div>Login</div>;
+    const navigate = useNavigate();
+
+    const [inputs, setInputs] = useState({
+        fname: "",
+        lname: "",
+        email: "",
+        password: "",
+    });
+
+    const handleInputsChange = (e) => {
+        setInputs((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const sendRequest = async () => {
+        const res = await axios
+            .post(`${process.env.REACT_APP_SERVER_URI}/auth/login`, {
+                email: inputs.email,
+                password: inputs.password,
+            })
+            .catch((err) => console.log(err));
+
+        const data = await res.data;
+
+        return data;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        sendRequest().then(() => navigate("/"));
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <Box
+                    display="flex"
+                    flexDirection="column"
+                    width={300}
+                    marginLeft="auto"
+                    marginRight="auto"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Typography variant="h2">Log in!</Typography>
+                    <TextField
+                        name="email"
+                        type={"email"}
+                        value={inputs.email}
+                        margin="normal"
+                        variant="outlined"
+                        placeholder="Email"
+                        onChange={handleInputsChange}
+                    />
+                    <TextField
+                        name="password"
+                        type={"password"}
+                        value={inputs.password}
+                        margin="normal"
+                        variant="outlined"
+                        placeholder="Password"
+                        onChange={handleInputsChange}
+                    />
+                    <Button type="submit" variant="contained">
+                        Log in
+                    </Button>
+                </Box>
+            </form>
+        </div>
+    );
 };
 
 export default Login;
