e888bfbfe4555868288ba1b0194fbf598f5b300a
Gaston <gaston.quittner@gmail.com>
2023-01-29 18:02:38 -0600

    React Router and links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-        </div>
+        <>
+            <header>
+                <Header />
+            </header>
+            <main>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/user" element={<Home />} />
+                </Routes>
+            </main>
+        </>
     );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,13 @@
 import { AppBar, Box, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     const [value, setValue] = useState();
 
     return (
         <div>
-            <AppBar>
+            <AppBar position="sticky">
                 <Toolbar>
                     <Typography variant="h3">MERN Auth</Typography>
                     <Box sx={{ marginLeft: "auto" }}>
@@ -16,8 +17,8 @@ const Header = () => {
                             value={value}
                             onChange={(e, val) => setValue(val)}
                         >
-                            <Tab label="Login" />
-                            <Tab label="Sign Up" />
+                            <Tab label="Login" LinkComponent={Link} to="/login" />
+                            <Tab label="Sign Up" LinkComponent={Link} to="/signup" />
                         </Tabs>
                     </Box>
                 </Toolbar>
