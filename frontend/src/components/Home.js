de34f12d9df975dc339c28e6d8e45aa2b18438fa
Gaston <gaston.quittner@gmail.com>
2023-02-04 20:43:15 -0600

    Added Token refresh

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,23 +3,46 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
+let firstRender = true;
+
 const Home = () => {
     const [user, setUser] = useState();
 
-    const sendRequest = async () => {
+    const refreshToken = async () => {
+        const res = await axios
+            .get(`${process.env.REACT_APP_SERVER_URI}/auth/refresh`, {
+                withCredentials: true,
+            })
+            .catch((err) => console.error(err));
+        const data = await res.data;
+
+        return data;
+    };
+
+    const getUser = async () => {
         const res = await axios
             .get(`${process.env.REACT_APP_SERVER_URI}/users/`, {
                 withCredentials: true,
             })
             .catch((err) => console.log(err));
 
-        const data = await res.data;
+        const user = await res.data;
+        console.log(user);
 
-        return data;
+        return user;
     };
 
     useEffect(() => {
-        sendRequest().then((data) => setUser(data));
+        if (firstRender) {
+            firstRender = false;
+            getUser().then((data) => setUser(data));
+        }
+
+        let interval = setInterval(() => {
+            refreshToken().then((data) => setUser(data));
+        }, 1000 * 15);
+
+        return () => clearInterval(interval);
     }, []);
 
     return <div>{user && <h1>{user.fname}</h1>}</div>;
